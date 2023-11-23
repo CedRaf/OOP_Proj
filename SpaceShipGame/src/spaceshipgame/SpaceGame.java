07c2720aec9886ce83d143b0456c1f61326ab5c4
Shabimbawa <133776210+Shabimbawa@users.noreply.github.com>
2023-11-23 18:28:09 +0800

    Update SpaceGame.java

--- SpaceShipGame/src/spaceshipgame/SpaceGame.java
+++ SpaceShipGame/src/spaceshipgame/SpaceGame.java
@@ -86,7 +86,51 @@ public class SpaceGame extends Application{
 }
    
         
+        public class Enemy extends Spaceship {
+        private int SPEED = (score/5)+2;
+
+        public Enemy(int posX, int posY, int size, Image image){
+            super(posX, posY, size, image);
+        }
+        public void update(){
+            super.update();
+            if(!exploding &&  !destroyed) posY +=SPEED;
+            if(posY > HEIGHT) destroyed = true;
+        } 
+
+        }
         
-    
-    
+
+        public class Bullet {
+            public boolean toRemove;
+
+            int postX, posY, speed = 10;
+            static final int size= 6;
+
+            public Bullet(int posX, int posY){
+                   this.postX=posX;
+                   this.posY=posY;
+            }
+            public void update(){
+                posY-=speed;
+            }
+            public void draw(){
+                gc.setFill(Color:RED);
+                if(score >=50 $$ score <=70 || scire >=120){
+                    gc.setFill(Color.YELLOWGREEN);
+                    speed =50;
+                    gc.fillRect(posX-5, posY-10, size+16, size+30);
+                }else{
+                   gc.fillOval(posX, posY, size, size);
+                }
+            }
+            
+        public boolean colide(Spaceship Spaceship) {
+            int distance = distance(this.posX +size /2, this.posY +size/2,
+                    Spaceship.posX +Spaceship.size /2, Spaceship.posY+Spaceship.size/2);
+            return distance < Spaceship.size /2 +size/2;
+        }
+
+        }
+
 }
