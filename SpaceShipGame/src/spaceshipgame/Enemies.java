6f42fa494c32f2dac22c8245417712be751ca14a
Ced <cedricrafanan27@gmail.com>
2023-11-23 17:04:52 +0800

    Initial Stuff

---
+++ SpaceShipGame/src/spaceshipgame/Enemies.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package spaceshipgame;
+
+import javafx.application.Application;
+
+/**
+ *
+ * @author Cedric
+ */
+public class Enemies extends Application{
+    
+}
